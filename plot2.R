2d58aa1eb981973be95bf7fccf8ef00b47ed027b
sashas-loot <sasha-kaun@gmx.de>
2020-08-23 16:59:02 +0200

    plot upload

--- plot2.R
+++ plot2.R
@@ -1,16 +1,16 @@
 library(lubridate)
 
-url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(url, destfile = "data.zip", method = "curl")
-unzip("data.zip")
-
-data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
-
-data$Date <- dmy(data$Date)
-
-my_data <- subset(data, Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02"))
-
-my_data$Date <- ymd(my_data$Date) + hms(my_data$Time)
+# url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+# download.file(url, destfile = "data.zip", method = "curl")
+# unzip("data.zip")
+# 
+# data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+# 
+# data$Date <- dmy(data$Date)
+# 
+# my_data <- subset(data, Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02"))
+# 
+# my_data$Date <- ymd(my_data$Date) + hms(my_data$Time)
 
 png("plot2.png")
 
